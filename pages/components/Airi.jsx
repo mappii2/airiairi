323d1b1b1592b5e8c0beff4fe0f56ca15562f1e7
増川 <mappiinex210@gmail.com>
2022-04-13 22:28:03 +0900

    airi

---
+++ pages/components/Airi.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components';
+
+export const Airi = () => {
+
+  const [airi, setAiri] = useState(["あいり！！"]);
+  const [isShow, setIsShow] = useState(false);
+
+
+  useEffect(() => {
+    let newArr = airi;
+    newArr.push("あいり！！");
+    setAiri(newArr);
+  }, [isShow]);
+
+  useEffect(() => {
+    let index = 0, num = 99999;
+    while (index < num) {
+      setIsShow(!isShow);
+      index++;
+    }
+  }, [isShow]);
+
+  return (
+    <>
+      {airi.map((item, index) => (
+        <WhiteText key={index}>{item}</WhiteText>
+      ))}
+    </>
+  );
+}
+
+const WhiteText = styled.span`
+  color: #fff;
+`;
